84052b5bdf0d50d14b49688940db6b0b815735d5
Denny Alfath <dennyalfath812@gmail.com>
2020-05-18 09:46:58 +0700

    Added info screen

---
+++ POV-CameraApp/GoldenTriangleVC.swift
@@ -0,0 +1,34 @@
+//
+//  GoldenTriangleVC.swift
+//  POV-CameraApp
+//
+//  Created by Denny Alfath on 15/05/20.
+//  Copyright © 2020 Denny Alfath. All rights reserved.
+//
+
+import UIKit
+
+class GoldenTriangleVC: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
